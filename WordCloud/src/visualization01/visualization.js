eeacf01fc4bad7aac946f615b24b2ef76dac823b
duncanolesen <dolesen@caci.co.uk>
2021-05-14 16:53:15 +0100

    Test Word Cloud in MooD

--- WordCloud/src/visualization01/visualization.js
+++ WordCloud/src/visualization01/visualization.js
@@ -1,6 +1,6 @@
 //
-// D3 scatter plot example
-// https://www.d3-graph-gallery.com/graph/scatter_basic.html
+// D3 word cloud
+// https://www.d3-graph-gallery.com/graph/wordcloud_size.html
 //
 // ** MooD BA customisation - start **
 //    Entry function declaration
@@ -15,17 +15,16 @@ export function visualization(config) {
   //
   // Retrieve graph configuration
   //
-  var inputs = config.inputs;
-  var xMin = inputs.xAxisMin;
-  var xMax = inputs.xAxisMax;
-  var yMin = inputs.yAxisMin;
-  var yMax = inputs.yAxisMax;
-  var style = config.style;
-  var margin = style.margin;
-  var width = parseFloat(config.width) - margin.left - margin.right;
-  var height = parseFloat(config.height) - margin.top - margin.bottom;
-  var spotRadius = style.spotRadius;
-  var fillColour = style.fillColour;
+  let inputs = config.inputs;
+  let style = config.style;
+  let margin = style.margin;
+  const width = parseFloat(config.width) - margin.left - margin.right;
+  const height = parseFloat(config.height) - margin.top - margin.bottom;
+  style.minWordLength = Math.max(1, style.minWordLength)
+  style.minInstanceCount = Math.max(1, style.minInstanceCount)
+  style.baseFontSize = style.baseFontSize || 20
+  style.instanceMultiplier = style.instanceMultiplier || 10
+
   //
   // ** MooD BA customisation - end **
   //
@@ -36,13 +35,13 @@ export function visualization(config) {
   //
   // set the dimensions and margins of the graph
   // append the svg object to the body of the page
-  var svg = d3.select("#" + config.element)
+  let svg = d3.select("#" + config.element)
     .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+            "translate(" + margin.left + "," + margin.top + ")")
   //
   // ** MooD BA customisation - end **
   //
@@ -51,17 +50,53 @@ export function visualization(config) {
   // ** MooD BA customisation - start **
   //    Read the data
   //
-  var data = config.data.rows
-  var myWords = [{word: "Running", size: 10}, {word: "Surfing", size: 20}, {word: "Climbing", size: 50}, {word: "Kiting", size: 30}, {word: "Sailing", size: 20}, {word: "Snowboarding", size: 60} ]
-//
+  let data = config.data.rows
+  let wordSplit = /[ !-\/:-@\[-`{-~]/
+  let numberPattern = /^-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?$/i 
+  // console.log(JSON.stringify(config))
+  let allWords = []
+  //
+  // Build array of all words in the text and filter out numbers and words considered too short
+  // and capitalise first letter
+  //
+  data.forEach(row => allWords = allWords
+    .concat((row.text ? row.text.split(wordSplit) : [])
+    .filter(word => !numberPattern.test(word) && word.length >= style.minWordLength)
+    .map(word => word[0].toUpperCase() + word.substring(1))
+    )
+  )
+  //
+  // Sort words into alphabetical order
+  //
+  allWords.sort()
+  //
+  // Build an array of unique words and a count of the number of occurences of each word
+  // and filter out words that don't occur often enough
+  //
+  const uniqueWords = allWords.reduce(function(accumulator, value, index, array) {
+    if (index > 0 && value === array[index-1]) {
+      accumulator[accumulator.length-1].count++ // matching word so increment instance count
+    } else {
+      accumulator.push({word: value, count: 1})
+    }
+    return accumulator
+  }, [])
+  //
+  // Calculate font size for words based on the number of occurences,
+  // filtering out words that don't occur often enough
+  //
+  const wordsOfInterest = uniqueWords
+    .filter(word => word.count >= style.minInstanceCount)
+    .map(function(word) {return {...word, size: style.baseFontSize + (word.count - style.minInstanceCount) * style.instanceMultiplier}} )
+  //
   // ** MooD BA customisation - end **
   //
 
   // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
   // Wordcloud features that are different from one word to the other must be here
-  var layout = d3Cloud()
+  let layout = d3Cloud()
     .size([width, height])
-    .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+    .words(wordsOfInterest.map(function(d) { return {text: d.word, size:d.size}; }))
     .padding(5)        //space between words
     .rotate(function() { return ~~(Math.random() * 2) * 90; })
     .fontSize(function(d) { return d.size; })      // font size of words
