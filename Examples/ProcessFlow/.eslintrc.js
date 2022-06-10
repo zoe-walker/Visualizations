b4b745ffc6b23cc8a2f4556dda53dad03b2a70d6
duncanolesen <dolesen@caci.co.uk>
2022-06-10 15:10:40 +0100

    Enable ESLint static code analysis on Process Flow

---
+++ Examples/ProcessFlow/.eslintrc.js
@@ -0,0 +1,17 @@
+// See
+// https://eslint.org/docs/user-guide/configuring/ and
+// https://eslint.org/docs/user-guide/configuring/configuration-files#extending-configuration-files
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "ignorePatterns": ["**/__tests__/*.js"],
+    "rules": {
+    }
+}
