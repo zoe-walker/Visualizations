dd99349b2be06df43b5e8d868a4d62c46f253217
duncanolesen <dolesen@caci.co.uk>
2022-07-20 12:16:13 +0100

    Standardise Jest unit test examples
    Add examples where previously missing in templates

---
+++ Templates/WebpackReactVisualizationTemplate/src/visualization01/__tests__/example.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// Put your JEST tests in here.
+
+describe('Simple test', () => {  
+    it('works', () => {
+      expect(0).toBe(0)
+    })
+    it('fails', () => {
+      expect(1).toBe(0)
+    })
+  })
+  
