5924cd77e64cffbb27d80a11ac56dbdfc49901d5
duncanolesen <dolesen@caci.co.uk>
2022-08-11 17:25:51 +0100

    Update stale Process Flow test data

--- Examples/ProcessFlow/test/visualization01/common-config.js
+++ Examples/ProcessFlow/test/visualization01/common-config.js
@@ -1,14 +1,13 @@
-// import dataConfig from './data.json';
-//import styleConfig from './style.json';
-// import dataConfig from './data2.json';
 // import dataConfig from './data-bp001.json';
 // import dataConfig from './data-bp025.json';
 import dataConfig from './data-bp025-redrawn.json';
 // import dataConfig from './data-bp027.json';
 // import dataConfig from './data-bp100.json';
+// import dataConfig from './data-bp101.json';
 // import dataConfig from './data-bp108.json';
 // import dataConfig from './data-bp124.json';
 // import dataConfig from './data-bp135.json';
+// import dataConfig from './data-bp150.json';
 // import dataConfig from './data-bpXYZ.json';
 // import dataConfig from './data-mro1-3 compact.json';
 // import dataConfig from './data-mro1-3.json';
