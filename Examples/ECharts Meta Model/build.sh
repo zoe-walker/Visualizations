084892d218159d53ed394a99af5e5991ab2426ba
duncanolesen <dolesen@caci.co.uk>
2022-08-18 15:42:10 +0100

    Correct zip command
    Use -r recursive option to traverse directories

--- Examples/ECharts Meta Model/build.sh
+++ Examples/ECharts Meta Model/build.sh
@@ -1,3 +1,3 @@
 packageversion=`grep version package.json | sed -e s/^.*:// -e s/,$// -e s/\"//g -e s/[.]/-/g`
 packagefilename=echarts-meta-model-${packageversion}
-zip ${packagefilename} package.json ECharts Meta\ Model
+zip -r ${packagefilename} package.json ECharts Meta\ Model
