d2694a2233308b202b03c7f714600290a8c3012c
duncanolesen <dolesen@caci.co.uk>
2022-08-18 11:19:25 +0100

    Add Build & Test and Vulnerability check workflows (#24)
    * Add initial build action
    
    Test using GitHub actions to build and test ProcessFlow
    
    * Correct action name
    
    * Correct action path
    
    * Correct step definition
    
    * Fix relative paths
    
    * Fix NodeJS version number
    
    * Fix action file structure
    
    * Refactor working directory
    
    * Try explicit change directory
    
    * Include working directory in lock file path
    
    * Dynamically build lock file name
    
    * Test code style check failure
    
    * Check Jest test failure
    
    * Undo Jest test failure
    
    * Try npm ci instead of npm install
    
    Check relative performance
    
    * Add workflow to check vulnerabilities
    
    * Add all webpack examples to build workflows
    
    * Add vulnerability checks for production only
    
    * Update check workflow
    
    * Split vulnerability check workflows
    
    * Try adjusting vulnerability schedule
    
    also rename jobs to be unique
    
    * Correct schedule syntax
    
    Also reduce workflow name length
    
    * Only run build workflow when code changed
    
    * Try an early run of check production workflow
    
    * Add checks for templates
    
    Also trigger production check workflow on push event to get it in the list of actions in GitHub UI
    
    * Add template checks for all dependencies workflow

--- Examples/EChartsRadialTree/src/visualization01/__tests__/example.test.js
+++ Examples/EChartsRadialTree/src/visualization01/__tests__/example.test.js
@@ -8,8 +8,8 @@ describe('Simple test', () => {
     it('works', () => {
       expect(0).toBe(0)
     })
-    it('fails', () => {
-      expect(1).toBe(0)
-    })
+    // it('fails', () => {
+    //   expect(1).toBe(0)
+    // })
   })
   
