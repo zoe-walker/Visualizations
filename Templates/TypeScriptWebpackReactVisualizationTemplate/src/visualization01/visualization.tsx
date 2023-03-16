2eed2f6365b9edbd702027d26562ed114037ed88
Josh Fenlon <jfenlon@caci.co.uk>
2023-03-16 14:26:28 +0000

    Fixed main visualization css not being included

--- Templates/TypeScriptWebpackReactVisualizationTemplate/src/visualization01/visualization.tsx
+++ Templates/TypeScriptWebpackReactVisualizationTemplate/src/visualization01/visualization.tsx
@@ -6,6 +6,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Logger from "@helpers/logger";
+import "./visualization.css";
 
 //
 //    Entry function declaration
